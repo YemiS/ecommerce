ed29b64c8582c8519e586510d364a79864e7707e
YemiS <yemi.jaiyeola@btinternet.com>
2021-03-21 00:38:35 +0000

    change port

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'mysql',
         host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
+        port: env.int('DATABASE_PORT', 3305),
         database: env('DATABASE_NAME', 'strapi-db'),
         username: env('DATABASE_USERNAME', 'rhodia'),
         password: env('DATABASE_PASSWORD', 'snookpassword'),
