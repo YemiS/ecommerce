655a3711dc13d20e4df20d319a48a3c4d48d5913
YemiS <yemi.jaiyeola@btinternet.com>
2021-03-21 14:45:09 +0000

    db name change

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', 'strapi-db.cns4g3fkdorf.eu-west-2.rds.amazonaws.com'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi-db'),
+        database: env('DATABASE_NAME', 'Strapi'),
         username: env('DATABASE_USERNAME', 'rhodia'),
         password: env('DATABASE_PASSWORD', 'snookpassword'),
         ssl: env.bool('DATABASE_SSL', false),
